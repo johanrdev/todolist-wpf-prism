b0b25a3fb373fabf9551d26edf7913099a8c8687
johanrdev <johanrothman1988@gmail.com>
2022-08-16 19:12:12 +0200

    Added ListView to TodosView and created a TodoItem model

---
+++ WPFTodoList/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Mvvm;
+
+namespace WPFTodoList.Models
+{
+    public class TodoItem : BindableBase
+    {
+        private string _title;
+        private bool _isCompleted;
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set => SetProperty(ref _isCompleted, value);
+        }
+    }
+}
--- WPFTodoList/ViewModels/TodosViewModel.cs
+++ WPFTodoList/ViewModels/TodosViewModel.cs
@@ -1,9 +1,21 @@
 ﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using WPFTodoList.Models;
 
 namespace WPFTodoList.ViewModels
 {
     public class TodosViewModel : BindableBase
     {
+        public ObservableCollection<TodoItem> Todos { get; set; }
 
+        public TodosViewModel()
+        {
+            Todos = new ObservableCollection<TodoItem>
+            {
+                new TodoItem { Title = "My First Todo" },
+                new TodoItem { Title = "My Second Todo" },
+                new TodoItem { Title = "My Third Todo" }
+            };
+        }
     }
 }
